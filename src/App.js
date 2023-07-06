dc11f01485a1a8fdddd73c8b67dd13ba4042bc22
ekram617161 <ekram@opltech.com>
2023-07-06 18:25:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
+import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null)
+
+  // // normal fetch api 
+
+  // const fetchMovieHandler = () => {
+  //   fetch('https://swapi.dev/api/films')
+  //     .then((response) => {
+  //       return response.json();
+  //     }).then((data) => {
+  //       const transformedMovies = data.results.map((moviesData) => {
+  //         return {
+  //           id: moviesData.episode_id,
+  //           title: moviesData.title,
+  //           openingText: moviesData.opening_crawl,
+  //           releaseDate: moviesData.release_date,
+  //         }
+  //       })
+  //       setMovies(transformedMovies)
+  //     })
+  // }
+
+  //  async await fetch api
+
+  useEffect(() => {
+    fetchMovieHandler();
+  }, [])
+
+  const fetchMovieHandler = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+
+      const response = await fetch('https://swapi.dev/api/films');
+      if (!response.ok) {
+        throw new Error('something went wrong!')
+      }
+      const data = await response.json()
+
+      const transformedMovies = data.results.map((moviesData) => {
+        return {
+          id: moviesData.episode_id,
+          title: moviesData.title,
+          openingText: moviesData.opening_crawl,
+          releaseDate: moviesData.release_date,
+        }
+      })
+      setMovies(transformedMovies);
+    }
+    catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+
+  }
+
+  let content = <p>Found no movies</p>
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />
+  }
+
+  if (error) {
+    content = <p>{error}</p>
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {/* {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>Found No movies</p>}
+        {!isLoading && error && <p>{error}</p>}
+        {isLoading && <p>Loading...</p>} */}
+
+        {content}
+
+      </section>
+    </React.Fragment>
   );
 }
 
